4e932ae4496742c08887daf67421ec28c7657c8f
inassjunus <inassjunus@gmail.com>
2025-04-14 12:53:27 +0700

    add admin column

--- db/migrate/20250413140712_create_users.rb
+++ db/migrate/20250413140712_create_users.rb
@@ -1,9 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[8.0]
   def change
     create_table :users, id: :uuid do |t|
-      t.string :email
-      t.string :name
-      t.string :password_digest
+      t.string :email, null: false, default: ""
+      t.string :name, null: false, default: ""
+      t.string :password_digest, null: false
+      t.boolean :admin, default: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 0) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_13_140712) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
+  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "name"
+    t.string "password_digest"
+    t.boolean "admin", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
